22b7d80cf087533c257ad98363b023799b6ea36e
Teemu H <swd1tn002@users.noreply.github.com>
2021-08-19 15:43:11 +0300

    Upotettujen videoide ponnahduslinkit

--- scripts.js
+++ scripts.js
@@ -4,3 +4,35 @@ tocbot.init({
     disableTocScrollSync: true,
     headingSelector: 'h1'
 });
+
+function openPopUp(url) {
+    window.open(url, 'videoPopUp', `toolbar=no,
+                                    location=no,
+                                    status=no,
+                                    menubar=no,
+                                    scrollbars=yes,
+                                    resizable=yes,
+                                    width=640,
+                                    height=360`);
+}
+
+function addVideoPopOutLinks() {
+    let videoFrames = document.querySelectorAll("iframe[src^='https://web.microsoftstream.com']")
+
+    videoFrames.forEach(frame => {
+        let url = frame.src;
+        let popOutLink = document.createElement('a');
+        popOutLink.class = 'popOutLink';
+        popOutLink.innerHTML = '🗗';
+        popOutLink.title = 'Avaa uudessa ikkunassa';
+        popOutLink.href = url;
+        popOutLink.onclick = (e) => {
+            e.preventDefault();
+            openPopUp(url);
+            return false;
+        }
+        frame.parentNode.insertBefore(popOutLink, frame.nextSibling);
+    });
+}
+
+addVideoPopOutLinks();
